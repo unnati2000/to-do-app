655ba821b578c6271f99f62e26434e548a9b82ca
unnati2000 <unnatibamania8@gmail.com>
2021-08-24 13:20:17 +0530

    Create onclick functions

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const listBox = document.getElementById("listBox");
 const todoArray = [];
 let id = 0;
 
+const deleteItem = (id) => {
+  console.log(id);
+};
+
 add.addEventListener("click", (e) => {
   e.preventDefault();
 
   console.log(text.value);
   todoArray.push({ text: text.value, id: id + 1 });
-  console.log(todoArray);
+  id = id + 1;
 
   text.value = "";
 
@@ -26,11 +30,11 @@ add.addEventListener("click", (e) => {
     todoDiv.append(todoTitle);
 
     // edit button
-
     const todoEditButton = document.createElement("button");
     todoEditButton.className =
       "p-2 lg:px-4 md:mx-2 text-white rounded bg-green-600";
     todoEditButton.innerText = "Edit";
+    todoEditButton.id = todoArray[i].id;
     todoDiv.append(todoEditButton);
 
     // delete button
@@ -39,6 +43,8 @@ add.addEventListener("click", (e) => {
     todoDeleteButton.className =
       "p-2 lg:px-4 md:mx-2 text-white rounded bg-red-600";
     todoDeleteButton.innerText = "Delete";
+    todoDeleteButton.id = todoArray[i].id;
+    todoDeleteButton.onclick = deleteItem(todoArray[i].id);
     todoDiv.append(todoDeleteButton);
 
     listBox.append(todoDiv);
